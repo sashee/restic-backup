8c9cfb8a0cd3e7d167472d7a3c13bdb90eaa92d3
sashee <tamas.sallai@advancedweb.hu>
2024-09-02 18:36:20 +0200

    monitoring

--- clients/index.js
+++ clients/index.js
@@ -25,31 +25,35 @@ const constructURL = (base, pathname, searchParams) => {
 
 console.log(JSON.stringify(process.env, undefined, 4));
 const runId = crypto.randomUUID();
+
+const monitoringAwsSecretAccessKey = await fs.readFile(path.join(process.env.CREDENTIALS_DIRECTORY, "monitoring_aws_secret_access_key"));
+const aws = new AwsClient({accessKeyId: process.env.MONITORING_AWS_ACCESS_KEY_ID, secretAccessKey: monitoringAwsSecretAccessKey});
+const sendMonitoring = async ({pathname, searchParams, method, body}) => {
+	const res = await aws.fetch(constructURL(process.env.MONITORING_URL, pathname, searchParams), {
+		method,
+		body,
+		aws: {
+			service: "lambda",
+			region: process.env.MONITORING_REGION,
+		},
+	});
+	if (!res.ok) {
+		const restext = await res.text();
+		throw new Error(`Error sending monitoring event: ${restext}`);
+	}
+}
+
 const awsSecretAccessKey = await fs.readFile(path.join(process.env.CREDENTIALS_DIRECTORY, "aws_secret_access_key"));
-const aws = new AwsClient({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: awsSecretAccessKey});
+
 const runCommand = async ({label, command, args, env, isJson}) => {
 	try {
 		const {stdout, stderr} = await execFile(command, args, {env});
-		await aws.fetch(constructURL(process.env.MONITORING_URL, "/log", {runid: runId, format: "json", type: label}), {
-			method: "POST",
-			body: JSON.stringify({stdout: isJson ? JSON.parse(stdout) : stdout, stderr}),
-			aws: {
-				service: "lambda",
-				region: process.env.MONITORING_REGION,
-			},
-		});
+		await sendMonitoring({pathname: "/log", searchParams: {runid: runId, label}, method: "POST", body: JSON.stringify({stdout: isJson ? JSON.parse(stdout) : stdout, stderr})});
 	}catch(e) {
 		if (e.stdout !== undefined && e.stderr !== undefined) {
 			// error is thrown by the execFile
 			console.log(e.stderr, e.stderr);
-			await aws.fetch(constructURL(process.env.MONITORING_URL, "/log", {runid: runId, format: "json", type: `${label}-error`}), {
-				method: "POST",
-				body: JSON.stringify({stdout: e.stdout, stderr: e.stderr}),
-				aws: {
-					service: "lambda",
-					region: process.env.MONITORING_REGION,
-				},
-			});
+			await sendMonitoring({pathname: "/log", searchParams: {runid: runId, label: `${label}-error`}, method: "POST", body: JSON.stringify({stdout: e.stdout, stderr: e.stderr})});
 			throw e;
 		}else {
 			throw e;
@@ -57,13 +61,7 @@ const runCommand = async ({label, command, args, env, isJson}) => {
 	}
 }
 
-await aws.fetch(constructURL(process.env.MONITORING_URL, "/run", {runid: runId}), {
-	method: "GET",
-	aws: {
-		service: "lambda",
-		region: process.env.MONITORING_REGION,
-	},
-});
+await sendMonitoring({pathname: "/run", searchParams: {runid: runId, monitor: process.env.MONITORING_MONITOR_NAME}, method: "GET"});
 
 try {
 	const resticPassword = await fs.readFile(path.join(process.env.CREDENTIALS_DIRECTORY, "restic_password"));
@@ -110,26 +108,12 @@ try {
 		if (runSettled.status === "rejected") {
 			throw {...logsSettled.value, error: runSettled.reason};
 		}
-		await aws.fetch(constructURL(process.env.MONITORING_URL, "/log", {runid: runId, format: "json", type: "backup"}), {
-			method: "POST",
-			body: JSON.stringify({stdout: JSON.parse(logsSettled.value.stdout), stderr: logsSettled.value.stderr}),
-			aws: {
-				service: "lambda",
-				region: process.env.MONITORING_REGION,
-			},
-		});
+		await sendMonitoring({pathname: "/log", searchParams: {runid: runId, label: "backup"}, method: "POST", body: JSON.stringify({stdout: JSON.parse(logsSettled.value.stdout), stderr: logsSettled.value.stderr})});
 	}catch(e) {
 		if (e.stdout !== undefined && e.stderr !== undefined) {
 			// error is thrown by the spawn
 			console.log(e.stderr, e.stdout, e.error);
-			await aws.fetch(constructURL(process.env.MONITORING_URL, "/log", {runid: runId, format: "json", type: "backup-error"}), {
-				method: "POST",
-				body: JSON.stringify({stdout: e.stdout, stderr: e.stderr, error: e.error}),
-				aws: {
-					service: "lambda",
-					region: process.env.MONITORING_REGION,
-				},
-			});
+			await sendMonitoring({pathname: "/log", searchParams: {runid: runId, label: "backup-error"}, method: "POST", body: JSON.stringify({stdout: e.stdout, stderr: e.stderr, error: e.error})});
 			throw e;
 		}else {
 			throw e;
@@ -143,20 +127,8 @@ try {
 	await runCommand({label: "snapshots", command: "restic", args: ["snapshots", "--json"], env: {AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID, RESTIC_REPOSITORY: process.env.RESTIC_REPOSITORY, AWS_SECRET_ACCESS_KEY: awsSecretAccessKey, RESTIC_PASSWORD: resticPassword}, isJson: true});
 	await runCommand({label: "stats", command: "restic", args: ["stats", "latest", "--json"], env: {AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID, RESTIC_REPOSITORY: process.env.RESTIC_REPOSITORY, AWS_SECRET_ACCESS_KEY: awsSecretAccessKey, RESTIC_PASSWORD: resticPassword}, isJson: true});
 
-	await aws.fetch(constructURL(process.env.MONITORING_URL, "/success", {runid: runId}), {
-		method: "GET",
-		aws: {
-			service: "lambda",
-			region: process.env.MONITORING_REGION,
-		},
-	});
+	await sendMonitoring({pathname: "/success", searchParams: {runid: runId}, method: "GET"});
 }catch(e) {
-	await aws.fetch(constructURL(process.env.MONITORING_URL, "/fail", {runid: runId}), {
-		method: "GET",
-		aws: {
-			service: "lambda",
-			region: process.env.MONITORING_REGION,
-		},
-	});
+	await sendMonitoring({pathname: "/fail", searchParams: {runid: runId}, method: "GET"});
 	throw e;
 }
