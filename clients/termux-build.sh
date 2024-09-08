71ed1ed5235fe58887f39564dd789b4903783aef
sashee <tamas.sallai@advancedweb.hu>
2024-09-08 20:18:05 +0200

    .

--- clients/termux-build.sh
+++ clients/termux-build.sh
@@ -1,4 +1,7 @@
 termux_step_make_install() {
+	echo "installing!"
+	ls
+	ls -Rl
 	install -Dm700 8086tiny "$TERMUX_PREFIX"/libexec/8086tiny
 	install -Dm600 bios "$TERMUX_PREFIX"/share/8086tiny/bios.bin
 	install -Dm600 fd.img "$TERMUX_PREFIX"/share/8086tiny/dos.img
