54bc95ded0d5215b47aa6ed0ef698862827ff0ef
sashee <tamas.sallai@advancedweb.hu>
2024-09-09 12:18:20 +0200

    vars

--- clients/termux-build.sh
+++ clients/termux-build.sh
@@ -2,6 +2,9 @@ termux_step_make_install() {
 	echo "installing!"
 	ls
 	ls -Rl
+	ls -R $TERMUX_PREFIX
+	echo "$TERMUX_PKG_SRCDIR"
+	ls -R $TERMUX_PKG_SRCDIR
 	echo "$PKGNAME"
 	install -Dm700 8086tiny "$TERMUX_PREFIX"/libexec/8086tiny
 	install -Dm600 bios "$TERMUX_PREFIX"/share/8086tiny/bios.bin
