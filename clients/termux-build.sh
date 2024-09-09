e164f65a4b1140f04ec852fa5c7ddeadee6bf99c
sashee <tamas.sallai@advancedweb.hu>
2024-09-09 12:45:30 +0200

    ls

--- clients/termux-build.sh
+++ clients/termux-build.sh
@@ -12,6 +12,9 @@ termux_step_make_install() {
 	echo "$PKGNAME"
 	ls - R ./packages/$TERMUX_PKG_NAME/*.patch
 	ls - R ./packages/$PKGNAME
+	echo "=====5"
+	echo "$TERMUX_PKG_NAME"
+	ls - R ./packages/$TERMUX_PKG_NAME
 	install -Dm700 8086tiny "$TERMUX_PREFIX"/libexec/8086tiny
 	install -Dm600 bios "$TERMUX_PREFIX"/share/8086tiny/bios.bin
 	install -Dm600 fd.img "$TERMUX_PREFIX"/share/8086tiny/dos.img
