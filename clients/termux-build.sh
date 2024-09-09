c0b7a352277ae0ea21eeb9874f230c6fde4ea027
sashee <tamas.sallai@advancedweb.hu>
2024-09-09 13:22:17 +0200

    dirs

--- clients/termux-build.sh
+++ clients/termux-build.sh
@@ -8,9 +8,12 @@ termux_step_make_install() {
 	pwd
 	ls -R /home/builder/termux-packages/packages/$TERMUX_PKG_NAME
 	mkdir -p "$TERMUX_PREFIX/lib/$TERMUX_PKG_NAME"
-	cp -R /home/builder/termux-packages/packages/$TERMUX_PKG_NAME/usr/lib/$TERMUX_PKG_NAME "$TERMUX_PREFIX/lib/$TERMUX_PKG_NAME"
+	cp -R /home/builder/termux-packages/packages/$TERMUX_PKG_NAME/usr/lib/$TERMUX_PKG_NAME/* "$TERMUX_PREFIX/lib/$TERMUX_PKG_NAME"
 	mkdir -p "$TERMUX_PREFIX/bin"
 	ln -s ../lib/$TERMUX_PKG_NAME/index.js $TERMUX_PREFIX/bin/$TERMUX_PKG_NAME
-	ls -R $TERMUX_PREFIX
+	echo "lib"
+	ln "$TERMUX_PREFIX/lib/$TERMUS_PKG_NAME"
+	echo "bin"
+	ln "$TERMUX_PREFIX/bin"
 	echo $TERMUX_DEBDIR
 }
