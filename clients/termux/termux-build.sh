d05ddfc4679de06cbd96818c436c7274064e033c
sashee <tamas.sallai@advancedweb.hu>
2024-09-11 14:16:55 +0200

    add schedule script

--- clients/termux/termux-build.sh
+++ clients/termux/termux-build.sh
@@ -2,10 +2,20 @@ termux_step_make_install() {
 	mkdir -p "$TERMUX_PREFIX/lib/$TERMUX_PKG_NAME"
 	cp -R /home/builder/termux-packages/packages/$TERMUX_PKG_NAME/usr/lib/$TERMUX_PKG_NAME/* "$TERMUX_PREFIX/lib/$TERMUX_PKG_NAME"
 	mkdir -p "$TERMUX_PREFIX/bin"
+
 	cat << EOF > $TERMUX_PREFIX/bin/$TERMUX_PKG_NAME
 #!\$TERMUX_PREFIX/bin/bash
 echo "Running script!"
-RUN_IN_SHELL=true $TERMUX_PREFIX/lib/$TERMUX_PKG_NAME/index.js "\$@"
+case "\${1}" in
+	"schedule")
+		echo "Running schedule!"
+		termux-job-scheduler --period-ms 86400000 --script $TERMUX_PREFIX/bin/$TERMUX_PKG_NAME --network unmetered --persisted true
+
+		exit 0;;
+	*)
+		RUN_IN_SHELL=true $TERMUX_PREFIX/lib/$TERMUX_PKG_NAME/index.js "\$@"
+	;;
+esac
 EOF
 	chmod +x $TERMUX_PREFIX/bin/$TERMUX_PKG_NAME
 }
