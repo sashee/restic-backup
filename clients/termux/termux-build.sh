cda8ef32948c0188a986824c2fa48ace0ccffe35
sashee <tamas.sallai@advancedweb.hu>
2024-09-11 13:43:05 +0200

    escaping

--- clients/termux/termux-build.sh
+++ clients/termux/termux-build.sh
@@ -3,28 +3,28 @@ termux_step_make_install() {
 	cp -R /home/builder/termux-packages/packages/$TERMUX_PKG_NAME/usr/lib/$TERMUX_PKG_NAME/* "$TERMUX_PREFIX/lib/$TERMUX_PKG_NAME"
 	mkdir -p "$TERMUX_PREFIX/bin"
 	cat << EOF > $TERMUX_PREFIX/bin/$TERMUX_PKG_NAME
-#!$TERMUX_PREFIX/bin/bash
+#!\$TERMUX_PREFIX/bin/bash
 echo "Running script!"
-RUN_IN_SHELL=true $TERMUX_PREFIX/lib/$TERMUX_PKG_NAME/index.js "$@"
+RUN_IN_SHELL=true $TERMUX_PREFIX/lib/$TERMUX_PKG_NAME/index.js "\$@"
 EOF
 	chmod +x $TERMUX_PREFIX/bin/$TERMUX_PKG_NAME
 }
 
 termux_step_create_debscripts () {
 	cat << EOF > postinst
-#!$TERMUX_PREFIX/bin/bash
+#!\$TERMUX_PREFIX/bin/bash
 echo "Running postinst!"
 echo $TERMUX_PKG_NAME
-mkdir -p $TERMUX_PREFIX/var/$TERMUX_PKG_NAME
-echo "postinst called" >> $TERMUX_PREFIX/var/$TERMUX_PKG_NAME/log
+mkdir -p \$TERMUX_PREFIX/var/$TERMUX_PKG_NAME
+echo "postinst called" >> \$TERMUX_PREFIX/var/$TERMUX_PKG_NAME/log
 EOF
 	chmod 0755 postinst
 	cat << EOF > prerm
-#!$TERMUX_PREFIX/bin/bash
+#!\$TERMUX_PREFIX/bin/bash
 echo "Running prerm!"
 echo $TERMUX_PKG_NAME
-mkdir -p $TERMUX_PREFIX/var/$TERMUX_PKG_NAME
-echo "prerm called" >> $TERMUX_PREFIX/var/$TERMUX_PKG_NAME/log
+mkdir -p \$TERMUX_PREFIX/var/$TERMUX_PKG_NAME
+echo "prerm called" >> \$TERMUX_PREFIX/var/$TERMUX_PKG_NAME/log
 EOF
 	chmod 0755 prerm
 }
